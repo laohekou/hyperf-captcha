b36672648d311f9827e7b33017c17949540e58fa
xiongy <15810265860@163.com>
2022-06-08 16:44:10 +0800

    heperf

--- src/Support/helper.php
+++ src/Support/helper.php
@@ -4,7 +4,11 @@
 if (!function_exists('captcha_create')) {
     function captcha_create()
     {
-        $captcha = new \Xyu\HyperfCaptcha\Captcha(\Hyperf\Utils\ApplicationContext::getContainer()->get(\Hyperf\Contract\ConfigInterface::class), \Hyperf\Utils\ApplicationContext::getContainer()->get(\Hyperf\Contract\SessionInterface::class));
+        $captcha = new \Xyu\HyperfCaptcha\Captcha(
+            \Hyperf\Utils\ApplicationContext::getContainer()
+                ->get(\Hyperf\Contract\ConfigInterface::class),\Hyperf\Utils\ApplicationContext::getContainer()
+            ->get(Psr\SimpleCache\CacheInterface::class)
+        );
         return $captcha->create();
     }
 }
@@ -12,7 +16,11 @@ if (!function_exists('captcha_create')) {
 if (!function_exists('captcha_check')) {
     function captcha_check(string $code)
     {
-        $captcha = new \Xyu\HyperfCaptcha\Captcha(\Hyperf\Utils\ApplicationContext::getContainer()->get(\Hyperf\Contract\ConfigInterface::class), \Hyperf\Utils\ApplicationContext::getContainer()->get(\Hyperf\Contract\SessionInterface::class));
+        $captcha = new \Xyu\HyperfCaptcha\Captcha(
+            \Hyperf\Utils\ApplicationContext::getContainer()
+                ->get(\Hyperf\Contract\ConfigInterface::class),\Hyperf\Utils\ApplicationContext::getContainer()
+            ->get(Psr\SimpleCache\CacheInterface::class)
+        );
         return $captcha->check($code);
     }
 }
